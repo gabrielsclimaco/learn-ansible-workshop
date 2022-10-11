d9937bf93d849d334323d2279cf3ae60f0601f76
Gabriel Climaco <gabrielsclimaco@protonmail.com>
2022-10-11 10:06:28 -0300

    add project base

---
+++ ansible/files/dump.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS tablezada (
+  message varchar(255) NOT NULL
+);          
+
+INSERT INTO tablezada(message) VALUES('Ansible eh πk mlk');
+
+ALTER TABLE tablezada OWNER TO "app";
